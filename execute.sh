5a20d6a89d8949db4650a58d7d268375237a2d34
Marcelo Costa <mycelo19@gmail.com>
2019-09-07 12:43:27 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -13,7 +13,7 @@ echo "Behave finished executing."
 gcloud config set project YOUR_PROJECT
 
 # Add this service account to the root of your source repo dir. It's a SA with the Log writter and Custom VM delete Role.
-gcloud auth activate-service-account --key-file=/app/auth/batch-workload-log-writter.json
+gcloud auth activate-service-account --key-file=/app/auth/batch-workload-sa.json
 
 gcloud logging write batch-workload "[$(hostname)]Exit status from behave cmd: $status"
 gcloud logging write batch-workload "[$(hostname)]Output from behave cmd: $output"
